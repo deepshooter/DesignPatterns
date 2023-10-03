4dde89148d8d50aa388d0c8d651109765dc24d01
avinashmishra <avinashm720@gmail.com>
2023-10-03 08:29:45 +0530

    Add Builder DesignPattern Implementation

---
+++ app/src/main/java/com/deepshooter/designpatterns/creational/builder/ComponentTest.kt
@@ -0,0 +1,27 @@
+package com.deepshooter.designpatterns.creational.builder
+
+import org.junit.jupiter.api.Test
+
+class ComponentTest {
+
+    @Test
+    fun builderTest() {
+
+        val component = Component.Builder()
+            .setParam1("Some Value")
+            .setParam3(true)
+            .build()
+
+        println(component)
+        println(component.param1)
+        println(component.param3)
+        println(component.param2)
+
+        assert(component.param1 == "Some Value")
+        assert(component.param3 == true)
+        assert(component.param2 == null)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/deepshooter/designpatterns/creational/builder/Note.kt
@@ -0,0 +1,12 @@
+package com.deepshooter.designpatterns.creational.builder
+
+/**
+ *
+ * -----Builder-----
+ *
+ * -Used when we have multiple parameters to initialize
+ * -Ex. Alert Dialog in Android
+ * -For many parameters, it's impractical to build all constructors ( 5 Parameter combinations -> 120 constructors)
+ * -Kotlin Solves this problem partially with named parameters ( Does not work with Java Code )
+ *
+ */
